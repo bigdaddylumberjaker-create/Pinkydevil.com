2bf720b2a814b0569a1b62d24589ab43e313c3df
bigdaddylumberjaker-create <bigdaddylumberjaker@gmail.com>
2026-03-16 23:43:14 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
+const bg = document.querySelector(".checkerBackground");
 
+document.addEventListener("mousemove", (e) => {
+  let x = (e.clientX / window.innerWidth - 0.5) * 20;
+  let y = (e.clientY / window.innerHeight - 0.5) * 20;
+
+  bg.style.transform = `rotateX(${10 - y}deg) rotateY(${x}deg)`;
+});
