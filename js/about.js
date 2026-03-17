7d69dc77dc065c30cd4d0cef8df8ea09ca2b3075
bigdaddylumberjaker-create <bigdaddylumberjaker@gmail.com>
2026-03-16 23:59:28 -0400

    Update about.js

--- js/about.js
+++ js/about.js
@@ -1,3 +1,12 @@
 window.addEventListener("load", () => {
   document.querySelector(".about-container").style.opacity = "1";
 });
+window.addEventListener("load", () => {
+  const reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach((item, index) => {
+    setTimeout(() => {
+      item.classList.add("show");
+    }, 120 + index * 120);
+  });
+});
